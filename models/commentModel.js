8e8a759427678008a3e2c779c4cbc16757a2539c
yam <yambalas123@gmail.com>
2024-11-25 21:01:07 +0200

    add comments api

---
+++ models/commentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: { 
+    type: String, 
+    required: true 
+  },
+  sender: { 
+    type: String, 
+    required: true 
+  },
+  postId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Post', 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+});
+
+//module.exports = mongoose.model('Comment', commentSchema);
